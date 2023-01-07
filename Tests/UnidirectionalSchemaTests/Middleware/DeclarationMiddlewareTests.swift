cedb8f73f8b734dc98d08d0be61792cef82a08ed
Jason Prasad <jasongprasad@gmail.com>
2023-01-07 15:35:34 -0500

    WIP, mixing statements

--- Tests/UnidirectionalSchemaTests/Middleware/DeclarationMiddlewareTests.swift
+++ Tests/UnidirectionalSchemaTests/Middleware/DeclarationMiddlewareTests.swift
@@ -1,21 +1,32 @@
+import SwiftUI
 import UnidirectionalSchema
 import XCTest
 
 final class DeclarationMiddlewareTests: XCTestCase {
     func testDeclarationMiddlewareInCountStore() {
-        @DeclarationBuilder var declarations: some Declaration<Count> {
-            Take(Count.Add.self) { mutation in
-                Put(Count.Decrement(mutation.value))
+        @DeclarationBuilder var declarations: some Declaration<Counts> {
+            Take(Counts.Add.self) { mutation in
+                Put(Counts.Decrement(mutation.value, to: \.first))
             }
-            Put(Count.Add(10))
+            Put(Counts.Add(10, to: \.second))
             Select { state in
+                Put(Counts.Add(state.second.count + 10, to: \.first))
+            }
+            Select(\Counts.first) { state in
+                Take(Count.Decrement.self)
                 Put(Count.Add(state.count + 10))
             }
         }
     }
 }
 
-public protocol Declaration<State> {
+public protocol Statement {}
+
+public protocol StatementConvertible {}
+
+public struct AnyStatement {}
+
+public protocol Declaration<State>: StatementConvertible {
     associatedtype State
 }
 
@@ -28,12 +39,16 @@ public enum DeclarationBuilder {
 
 public struct Block<State>: Declaration {}
 
+public struct Call: Statement {}
+
 public struct Put<State>: Declaration {
     public init(_: any Mutation<State>) {}
 }
 
 public struct Select<State>: Declaration {
     public init(@DeclarationBuilder _: (State) -> any Declaration<State>) {}
+
+    init<Substate>(_: WritableKeyPath<State, Substate>, @DeclarationBuilder _: (Substate) -> any Declaration<Substate>) {}
 }
 
 public struct Take<State>: Declaration {
