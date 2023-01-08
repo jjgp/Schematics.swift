cda3bedde1edb27b4b3f5170588d96e3837b6cc6
Jason Prasad <jasongprasad@gmail.com>
2023-01-08 15:34:59 -0500

    Add CombineSchema

--- Package.swift
+++ Package.swift
@@ -9,12 +9,15 @@ let package = Package(
         .macOS(.v13)
     ],
     products: [
+        .library(name: "CombineSchema", targets: ["CombineSchema"]),
         .library(name: "FoundationSchema", targets: ["FoundationSchema"]),
         .library(name: "ReactiveSchema", targets: ["ReactiveSchema"]),
         .library(name: "UnidirectionalSchema", targets: ["UnidirectionalSchema"])
     ],
     dependencies: [],
     targets: [
+        .target(name: "CombineSchema", dependencies: []),
+        .testTarget(name: "CombineSchemaTests", dependencies: ["CombineSchema"]),
         .target(name: "FoundationSchema", dependencies: []),
         .testTarget(name: "FoundationSchemaTests", dependencies: ["FoundationSchema"]),
         .target(name: "ReactiveSchema", dependencies: ["FoundationSchema"]),
