dfe6ee1bb9e378c0d90d650f78caee8df6d4d2de
Jason Prasad <jasongprasad@gmail.com>
2023-03-23 10:04:24 -0400

    Switch order of operations, setup dispatch and then attach

--- Sources/UnidirectionalSchema/Store.swift
+++ Sources/UnidirectionalSchema/Store.swift
@@ -16,11 +16,11 @@ public final class Store<State>: Publisher, StateContainer {
         self.subject = subject
 
         if let middleware = middleware {
-            middleware.attachTo(eraseToAnyStateContainer())
-
             self.dispatch = { mutation in
                 middleware.respond(to: mutation, forwardingTo: dispatch)
             }
+
+            middleware.attachTo(eraseToAnyStateContainer())
         } else {
             self.dispatch = dispatch
         }
