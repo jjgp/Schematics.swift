dec14702f70fae7846415c6de79ace0122531ed3
Jason Prasad <jasongprasad@gmail.com>
2023-04-29 21:35:21 -0400

    Add throwing map

--- Sources/CombineSchema/Publishers+Async.swift
+++ Sources/CombineSchema/Publishers+Async.swift
@@ -11,12 +11,38 @@ public extension Publisher {
         .switchToLatest()
     }
 
+    func tryMapAsync<T>(priority: TaskPriority? = nil,
+                        operation: @escaping @Sendable (Output) async throws -> T) -> some Publisher<T, Error> {
+        mapError {
+            $0 as Error // TODO: not sure about this
+        }
+        .map { output in
+            AsyncThrowingPublisher(priority: priority, operation: {
+                try await operation(output)
+            })
+        }
+        .switchToLatest()
+    }
+
     func flatMapAsync<T>(maxPublishers: Subscribers.Demand = .unlimited,
                          priority: TaskPriority? = nil,
                          operation: @escaping @Sendable (Output) async -> T) -> some Publisher<T, Failure> {
         flatMap(maxPublishers: maxPublishers) { output in
             AsyncPublisher(priority: priority, operation: {
                 await operation(output)
+            })
+        }
+    }
+
+    func flatMapAsync<T>(maxPublishers: Subscribers.Demand = .unlimited,
+                         priority: TaskPriority? = nil,
+                         operation: @escaping @Sendable (Output) async throws -> T) -> some Publisher<T, Error> {
+        mapError {
+            $0 as Error // TODO: not sure about this
+        }
+        .flatMap(maxPublishers: maxPublishers) { output in
+            AsyncThrowingPublisher(priority: priority, operation: {
+                try await operation(output)
             })
         }
     }
